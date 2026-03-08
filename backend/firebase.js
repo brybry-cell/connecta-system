5337bcfaa891e333a60f1192004aa99b12ccefc9
Bryce <Bryce.obien17@gmail.com>
2026-03-09 01:16:06 +0800

    simplify firebase env config

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,11 +1,9 @@
 const admin = require("firebase-admin");
 
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
 admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.project_id,
-    clientEmail: process.env.client_email,
-    privateKey: process.env.private_key.replace(/\\n/g, "\n"),
-  }),
+  credential: admin.credential.cert(serviceAccount)
 });
 
 const db = admin.firestore();
